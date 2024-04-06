d51b98ed5d4ec9fb89687c8ec84f98db06c5678a
Student <student@ndmu.local>
2024-04-06 15:39:07 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+// 1
+import 'fooderlich_theme.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+// 2
+    final theme = FooderlichTheme.dark();
+// TODO: Apply Home widget
+return MaterialApp(
+theme: theme,
+title: 'Fooderlich',
+home: const Home(),
+);
+  }
+}
